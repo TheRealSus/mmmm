5b0b10b43f407f5e6ce04c04e259fd6531c41548
TheRealSus <145399190+TheRealSus@users.noreply.github.com>
2024-03-02 20:06:58 -0800

    Initial commit

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 20, -18);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes the camera follow the vehicle
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    //variable stuff
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity = -6.8f;
+
+    [SerializeField] private bool grounded;
+    [SerializeField] private float velocityY;
+    [SerializeField] private float jumpVelocity;
+
+    //update function
+    private void Update()
+    {
+        //player input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //jumping
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            velocityY = jumpVelocity;
+        }
+
+        //calculate vertical speed from gravity
+        if(!grounded)
+        {
+            velocityY += gravity * Time.deltaTime;
+        }
+
+        Vector3 vertical = new Vector3(0, velocityY, 0);
+
+        //movement
+        Vector3 direction = new Vector3(x, 0, z);
+        Vector3 move = direction + vertical;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //turn and face direction
+        if (x !=0 || z !=0)
+        {
+            float yRot = Mathf.Atan2(x, z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, yRot, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject.layer == 6)
+        {
+            grounded = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+       if(collider.gameObject.layer == 6)
+        {
+            velocityY = 0;
+            grounded = true;
+        }
+    }
+
+    private void Gravity()
+    {
+        if(!grounded)
+        {
+            velocityY += gravity * Time.deltaTime;
+        }
+    }
+
+}
