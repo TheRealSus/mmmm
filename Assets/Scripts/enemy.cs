8adef22acf86615d03c4293f388f23126fc99bc3
TheRealSus <lozada.josh@icloud.com>
2024-06-12 15:31:33 -0700

    running away from women
    jk

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] float enemySpeed;
+    [SerializeField] float distance; //distance of enemy to player
+    [SerializeField] float noticeDistance; //how far away you are before it sees you
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PLayer").transform;
+        startPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.position);
+        if(distance <= noticeDistance)
+        {
+           chase(); //chase
+        }
+        if(distance > noticeDistance)
+        {
+           goHome(); //go back home
+        }
+
+        void chase()
+        {
+            transform.LookAt(player);
+            transform.Translate(0,0, enemySpeed * Time.deltaTime);
+        }
+
+        void goHome()
+        {
+
+        }
+    }
+}
